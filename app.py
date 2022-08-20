e50bcd85c1437aff02398e6b28b11e53f7e93782
jagmeet-singh <jagmeet.malhotra@gmail.com>
2022-08-20 18:32:04 +0530

    Updated Weploy

--- app.py
+++ app.py
@@ -1,60 +1,132 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, abort, session, jsonify
 import json
 import os.path
+import os
 from werkzeug.utils import secure_filename
+import pandas as pd
+import fasttext
+import numpy as np
+import re
+import string
+import pickle
+fasttext.FastText.eprint = lambda x: None
 
-app = Flask(__name__)
+app= Flask(__name__)
 app.secret_key = 'h432hi5ohi3h5i5hi3o2hi'
+model = pickle.load(open('logreg_model_deploy.sav','rb'))
+
+encoder_mapping = {0: 'GRP_0', 1: 'GRP_12', 2: 'GRP_13', 3: 'GRP_14', 4: 'GRP_19', 5: 'GRP_2', 6: 'GRP_24', 7: 'GRP_25', 8: 'GRP_29', 9: 'GRP_3', 10: 'GRP_33', 11: 'GRP_4', 12: 'GRP_8', 13: 'GRP_MANUAL'}
+
+def clean_text_before_lang_detect(text  ):
+    #remove _x000D_
+    text = re.sub(r'_x000D_', '',text)
+    #remove tabs
+    text = re.sub(r'\t', '',text)
+    #remove puntuations
+    text = text.translate(str.maketrans('','',string.punctuation))
+    # Remove new line characters 
+    text = re.sub(r'\n',' ',text)
+    #Remove leading & trailing Spaces
+    text = re.sub(r"^\s+|\s+$", "", text)
+    # Remove numbers and special chaacters
+    text = re.sub(r'\d+','' ,text)
+    text = re.sub(r'\/\/',' ', text)
+    text = re.sub(r'\\',' ', text)
+    text = re.sub(r'\(',' ', text)
+    text = re.sub(r'\)',' ', text)
+    text = re.sub(r':' ,' ', text)
+    text = re.sub(r'-' ,' ', text)
+    text = re.sub(r'\/',' ', text)
+    text = re.sub(r'\.',' ', text)
+    text = re.sub(r'\,',' ', text)
+    #Remove email 
+    text = re.sub(r'\S*@\S*\s?', '', text)
+    # Remove hyperlinks
+    text = re.sub(r'https?:\/\/.*\/\w*', '', text)
+    # Remove hashtag while keeping hashtag text
+    text = re.sub(r'#','', text)
+    #remove '_'
+    text = re.sub(r'_',' ',text)
+    #remove additional spaces 
+    text = re.sub(' +', ' ', text)
+    text=text.lower()
+    text = text.strip()
+    return text
+
+
+def clean_data_domainContext(text  ):
+    text = re.sub(r"received from:",'',text)
+    text = re.sub(r"received:",'',text)
+    text = re.sub(r"hi",' ',text)
+    text = re.sub(r"this message was sent from an unmonitored email address",'',text)
+    text = re.sub(r"email:",' ',text)
+    text = re.sub(r"email address:",'',text)
+    text = re.sub(r"from:",' ',text)
+    text = re.sub(r"sddubject:",'',text)
+    text = re.sub(r"please do not reply to this message",' ',text)
+    text = re.sub(r"select the following link to view the disclaimer in an alternate language",' ',text)
+    text = re.sub(r"description problem",'',text)
+    text = re.sub(r"steps taken far",' ',text)
+    text = re.sub(r"please do the needful",'',text)
+    text = re.sub(r"customer job title",' ',text)
+    text = re.sub(r"sales engineer contact",'',text)
+    text = re.sub(r"please note that",' ',text)
+    text = re.sub(r"please find below",'',text)
+    text = re.sub(r"hello",'',text)
+    text = re.sub(r"date and time",' ',text)
+    text = re.sub(r"kindly refer mail:",'',text)
+    text = re.sub(r"name:",' ',text)
+    text = re.sub(r"language:",'',text)
+    text = re.sub(r"customer number:",' ',text)
+    text = re.sub(r"telephone:",'',text)
+    text = re.sub(r"summary:",' ',text)
+    text = re.sub(r"sincerely",'',text)
+    text = re.sub(r"company inc",' ',text)
+    text = re.sub(r"hallo",'',text)
+    text = re.sub(r"hi it team",' ',text)
+    text = re.sub(r"hi team",'',text)
+    text = re.sub(r" hi ",'',text)
+    text = re.sub(r"best",' ',text)
+    text = re.sub(r" na ",' ',text)
+    text = re.sub(r" yes ",' ',text)
+    text = re.sub(r" kind ",'',text)
+    text = re.sub(r" regards ",' ',text)
+    text = re.sub(r" good morning ",'',text)
+    text = re.sub(r" please ",'',text)
+    text = re.sub(r"monitoring_tool@company.com",'MonitoringTool',text)
+    #Remove email 
+    text = re.sub(r'\S*@\S*\s?', '', text)
+    text = text.translate(str.maketrans('','',string.punctuation))
+    text = re.sub(' +', ' ', text)
+    text = text.strip()
+    return text
 
 @app.route('/')
 def home():
     return render_template('home.html', codes=session.keys())
 
-@app.route('/your-url', methods=['GET','POST'])
-def your_url():
+@app.route('/your-ticket', methods=['GET','POST'])
+def your_ticket():
     if request.method == 'POST':
-        urls = {}
-
-        if os.path.exists('urls.json'):
-            with open('urls.json') as urls_file:
-                urls = json.load(urls_file)
-
-        if request.form['code'] in urls.keys():
-            flash('That short name has already been taken. Please select another name.')
-            return redirect(url_for('home'))
-
-        if 'url' in request.form.keys():
-            urls[request.form['code']] = {'url':request.form['url']}
-        else:
-            f = request.files['file']
-            full_name = request.form['code'] + secure_filename(f.filename)
-            f.save('./static/user_files/' + full_name)
-            urls[request.form['code']] = {'file':full_name}
-
-
-        with open('urls.json','w') as url_file:
-            json.dump(urls, url_file)
-            session[request.form['code']] = True
-        return render_template('your_url.html', code=request.form['code'])
+        sd =  request.form['sd']
+        desc = request.form['desc']
+        en_desc = sd + " " + desc
+        en_desc = re.sub(r'\n','',en_desc)
+        en_desc = clean_text_before_lang_detect(en_desc)
+        en_desc = clean_data_domainContext(en_desc)
+        result = model.predict([en_desc])
+        print("Result :"+ str(result));
+        flash("Your ticket has been assinged to Assignment Group : " + encoder_mapping[result[0]] )
+        return redirect(url_for('home'))
     else:
         return redirect(url_for('home'))
 
-@app.route('/<string:code>')
-def redirect_to_url(code):
-    if os.path.exists('urls.json'):
-        with open('urls.json') as urls_file:
-            urls = json.load(urls_file)
-            if code in urls.keys():
-                if 'url' in urls[code].keys():
-                    return redirect(urls[code]['url'])
-                else:
-                    return redirect(url_for('static', filename='user_files/' + urls[code]['file']))
-    return abort(404)
+
 
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
 
-@app.route('/api')
-def session_api():
-    return jsonify(list(session.keys()))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8080,debug=True)
